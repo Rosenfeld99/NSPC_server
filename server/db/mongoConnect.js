1020cfa8ff408fc599c65b150ea3e87f6d67e874
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2023-09-06 00:49:11 +0300

    Delete server directory

--- server/db/mongoConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { SECRET } = require("../config/secret");
-
-main().catch((err) => console.log(err));
-
-async function main() {
-  await mongoose.connect(SECRET.MONGO_CONNECTION);
-  // console.log("mongo connect idf8 local");
-}
